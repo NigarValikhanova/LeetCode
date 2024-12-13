2a31b6f7fbc397bdb6d75ca6c41b88cc50bf6716
Nigar <140783772+NigarValikhanova@users.noreply.github.com>
2024-12-13 16:18:42 +0400

    Add project files.

---
+++ LeetCode/Program.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode;
+
+public class Program
+{
+    public static void Main()
+    {
+        #region LeetCode1
+        Solution1 solution = new Solution1();
+
+        int[] nums1 = { 2, 7, 11, 15 };
+        int target1 = 9;
+        Console.WriteLine(string.Join(", ", solution.TwoSum(nums1, target1))); // Output: 0, 1
+
+        int[] nums2 = { 3, 2, 4 };
+        int target2 = 6;
+        Console.WriteLine(string.Join(", ", solution.TwoSum(nums2, target2))); // Output: 1, 2
+
+        int[] nums3 = { 3, 3 };
+        int target3 = 6;
+        Console.WriteLine(string.Join(", ", solution.TwoSum(nums3, target3))); // Output: 0, 1
+
+        #endregion
+    }
+}
---
+++ LeetCode/Solution1.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode
+{
+    public class Solution1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> complementMap = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = nums[i];
+
+                if (complementMap.ContainsKey(num))
+                {
+                    return new int[] { complementMap[num], i };
+                }
+
+                complementMap[target - num] = i;
+            }
+
+            return new int[0];
+        }
+    }
+}
