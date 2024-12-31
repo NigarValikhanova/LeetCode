3e4ba028b79d656d55110da15ded80c854ecf45b
Nigar <140783772+NigarValikhanova@users.noreply.github.com>
2024-12-31 18:30:13 +0400

    Updated version

--- LeetCode/Solution18.cs
+++ LeetCode/Solution18.cs
@@ -1,42 +1,36 @@
-using System;
-using System.Collections.Generic;
-
 public class Solution18
 {
     public IList<IList<int>> FourSum(int[] nums, int target)
     {
-        Array.Sort(nums);  // Sort the array to facilitate skipping duplicates.
+        Array.Sort(nums); // Sort the array to simplify handling duplicates
         var result = new List<IList<int>>();
-        
-        for (int i = 0; i < nums.Length - 3; i++)
-        {
-            // Skip duplicate elements for the first number
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
+        int n = nums.Length;
 
-            for (int j = i + 1; j < nums.Length - 2; j++)
+        for (int i = 0; i < n - 3; i++)
+        {
+            // Avoid duplicates for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < n - 2; j++)
             {
-                // Skip duplicate elements for the second number
-                if (j > i + 1 && nums[j] == nums[j - 1])
-                    continue;
+                // Avoid duplicates for the second element
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
 
                 int left = j + 1;
-                int right = nums.Length - 1;
-                
+                int right = n - 1;
+
                 while (left < right)
                 {
-                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
-                    
+                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+
                     if (sum == target)
                     {
                         result.Add(new List<int> { nums[i], nums[j], nums[left], nums[right] });
-                        
-                        // Skip duplicate elements for the third and fourth numbers
-                        while (left < right && nums[left] == nums[left + 1])
-                            left++;
-                        while (left < right && nums[right] == nums[right - 1])
-                            right--;
-                        
+
+                        // Move left and right pointers to avoid duplicates
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
                         left++;
                         right--;
                     }
@@ -51,7 +45,7 @@ public class Solution18
                 }
             }
         }
-        
+
         return result;
     }
 }
