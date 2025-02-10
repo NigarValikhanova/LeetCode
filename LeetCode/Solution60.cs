d556898118936b22242fc78db688ff6edc9d6807
Nigar <140783772+NigarValikhanova@users.noreply.github.com>
2025-02-10 17:50:17 +0400

    LeetCode Problem 60 (Hard Level)

---
+++ LeetCode/Solution60.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode
+{
+    public class Solution60
+    {
+        public string GetPermutation(int n, int k)
+        {
+            List<int> numbers = new List<int>();
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                numbers.Add(i);
+                fact *= i;
+            }
+
+            k--; // Convert to 0-based index
+            string result = "";
+
+            for (int i = n; i > 0; i--)
+            {
+                fact /= i; // Get factorial of (i-1)
+                int index = k / fact;
+                result += numbers[index];
+                numbers.RemoveAt(index);
+                k %= fact;
+            }
+
+            return result;
+        }
+
+    }
+}
