51ab6a19dec51ba8d85dd9dde180cfb99f8af8af
Nigar <140783772+NigarValikhanova@users.noreply.github.com>
2025-02-11 12:21:26 +0400

    LeetCode Problem 61 (Medium Level)

---
+++ LeetCode/Solution61.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class Solution61
+    {
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            if (head == null || head.next == null || k == 0)
+                return head;
+
+            // Find the length of the list
+            ListNode current = head;
+            int length = 1;
+            while (current.next != null)
+            {
+                current = current.next;
+                length++;
+            }
+
+            // Make the list circular
+            current.next = head;
+
+            // Find the new tail position
+            k = k % length;
+            int stepsToNewTail = length - k;
+
+            // Find the new tail and the new head
+            ListNode newTail = head;
+            for (int i = 1; i < stepsToNewTail; i++)
+            {
+                newTail = newTail.next;
+            }
+
+            ListNode newHead = newTail.next;
+            newTail.next = null; // Break the cycle
+
+            return newHead;
+        }
+    }
+
+}
