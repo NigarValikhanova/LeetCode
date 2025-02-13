f2bcdc753714747c2c99663758cd6e79cc27f747
Nigar <140783772+NigarValikhanova@users.noreply.github.com>
2025-02-13 22:32:03 +0400

    LeetCode Problem63 (Medium Level)

---
+++ LeetCode/Solution63.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode
+{
+    public class Solution63
+    {
+        public int UniquePathsWithObstacles(int[][] obstacleGrid)
+        {
+            int m = obstacleGrid.Length;
+            int n = obstacleGrid[0].Length;
+
+            if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1)
+                return 0; // If start or end cell has an obstacle, no path is possible.
+
+            int[,] dp = new int[m, n];
+            dp[0, 0] = 1;
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (obstacleGrid[i][j] == 1)
+                    {
+                        dp[i, j] = 0; // No path through an obstacle
+                    }
+                    else
+                    {
+                        if (i > 0) dp[i, j] += dp[i - 1, j];
+                        if (j > 0) dp[i, j] += dp[i, j - 1];
+                    }
+                }
+            }
+
+            return dp[m - 1, n - 1];
+        }
+    }
+}
