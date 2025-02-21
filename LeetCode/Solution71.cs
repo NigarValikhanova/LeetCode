360ea5c83206c7f1123d6807e57977ca477bd664
Nigar <140783772+NigarValikhanova@users.noreply.github.com>
2025-02-21 15:18:05 +0400

    LeetCode Problem 71 (Medium Level)

---
+++ LeetCode/Solution71.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode
+{
+    public class Solution71
+    {
+        public string SimplifyPath(string path)
+        {
+            Stack<string> stack = new Stack<string>();
+            string[] parts = path.Split('/');
+
+            foreach (string part in parts)
+            {
+                if (part == "..")
+                {
+                    if (stack.Count > 0)
+                        stack.Pop();
+                }
+                else if (!string.IsNullOrEmpty(part) && part != ".")
+                {
+                    stack.Push(part);
+                }
+            }
+
+            return "/" + string.Join("/", stack.ToArray().Reverse());
+        }
+    }
+}
