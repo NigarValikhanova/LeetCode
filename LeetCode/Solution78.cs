a80903eea7c12796dfc7681011bb5777ff137975
Nigar <140783772+NigarValikhanova@users.noreply.github.com>
2025-02-28 14:11:56 +0400

    LeetCode Problem 78 (Medium Level)

---
+++ LeetCode/Solution78.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class Solution78
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            var result = new List<IList<int>>();
+            GenerateSubsets(nums, 0, new List<int>(), result);
+            return result;
+        }
+
+        private void GenerateSubsets(int[] nums, int index, List<int> current, IList<IList<int>> result)
+        {
+            result.Add(new List<int>(current));
+
+            for (int i = index; i < nums.Length; i++)
+            {
+                current.Add(nums[i]);
+                GenerateSubsets(nums, i + 1, current, result);
+                current.RemoveAt(current.Count - 1);
+            }
+        }
+    }
+}
