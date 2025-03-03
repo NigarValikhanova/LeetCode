8132843ff02d9051b9fa9a57a32c6136d818c715
Nigar <140783772+NigarValikhanova@users.noreply.github.com>
2025-03-03 10:00:50 +0400

    LeetCode Problem 80 (Medium Level)

---
+++ LeetCode/Solution80.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode
+{
+    public class Solution80
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length <= 2) return nums.Length;
+
+            int index = 2; // Start from the third element
+
+            for (int i = 2; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[index - 2])
+                {
+                    nums[index] = nums[i];
+                    index++;
+                }
+            }
+
+            return index;
+        }
+    }
+}
