f55be227bb28a3be2390d22b7b810d1d9824b3ce
Nigar <140783772+NigarValikhanova@users.noreply.github.com>
2025-03-07 17:11:09 +0400

    LeetCode Problem85 (Hard Level)

---
+++ LeetCode/Solution85.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class Solution85
+    {
+        public int MaximalRectangle(char[][] matrix)
+        {
+            if (matrix == null || matrix.Length == 0 || matrix[0].Length == 0)
+                return 0;
+
+            int rows = matrix.Length, cols = matrix[0].Length;
+            int[] heights = new int[cols];
+            int maxArea = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    heights[j] = (matrix[i][j] == '1') ? heights[j] + 1 : 0;
+                }
+                maxArea = Math.Max(maxArea, LargestRectangleArea(heights));
+            }
+
+            return maxArea;
+        }
+
+        private int LargestRectangleArea(int[] heights)
+        {
+            Stack<int> stack = new Stack<int>();
+            int maxArea = 0;
+            int n = heights.Length;
+
+            for (int i = 0; i <= n; i++)
+            {
+                int h = (i == n) ? 0 : heights[i];
+                while (stack.Count > 0 && h < heights[stack.Peek()])
+                {
+                    int height = heights[stack.Pop()];
+                    int width = (stack.Count == 0) ? i : i - stack.Peek() - 1;
+                    maxArea = Math.Max(maxArea, height * width);
+                }
+                stack.Push(i);
+            }
+
+            return maxArea;
+        }
+    }
+
+}
