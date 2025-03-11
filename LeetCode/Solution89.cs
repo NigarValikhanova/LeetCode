4f73a83b803487838a376cd899cbfad2e84418c5
Nigar <140783772+NigarValikhanova@users.noreply.github.com>
2025-03-11 11:29:29 +0400

    LeetCode Problem89 (Medium Level)

---
+++ LeetCode/Solution89.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class Solution89
+    {
+        public IList<int> GrayCode(int n)
+        {
+            List<int> grayCode = new List<int>();
+            int numElements = 1 << n; // 2^n elements
+
+            for (int i = 0; i < numElements; i++)
+            {
+                grayCode.Add(i ^ (i >> 1));
+            }
+
+            return grayCode;
+        }
+    }
+}
