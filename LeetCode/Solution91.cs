49b930346b4f537d8f69268c8a5056224f4588f7
Nigar <140783772+NigarValikhanova@users.noreply.github.com>
2025-03-13 12:12:54 +0400

    LeetCode Problem 91 (Medium Level)

---
+++ LeetCode/Solution91.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class Solution91
+    {
+        public int NumDecodings(string s)
+        {
+            if (s == null || s.Length == 0 || s[0] == '0') return 0;
+
+            int n = s.Length;
+            int[] dp = new int[n + 1];
+
+            dp[0] = 1; // Base case: empty string has one way to decode
+            dp[1] = s[0] != '0' ? 1 : 0; // Single character decoding
+
+            for (int i = 2; i <= n; i++)
+            {
+                int oneDigit = int.Parse(s.Substring(i - 1, 1));
+                int twoDigits = int.Parse(s.Substring(i - 2, 2));
+
+                if (oneDigit >= 1)
+                {
+                    dp[i] += dp[i - 1];
+                }
+
+                if (twoDigits >= 10 && twoDigits <= 26)
+                {
+                    dp[i] += dp[i - 2];
+                }
+            }
+
+            return dp[n];
+        }
+    }
+
+}
