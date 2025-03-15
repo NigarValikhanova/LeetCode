c7ce0feb3f09687ba3b69f5a974c2c105ab822f3
Nigar <140783772+NigarValikhanova@users.noreply.github.com>
2025-03-15 13:16:28 +0400

    LeetCode Problem 93 (Medium Level)

---
+++ LeetCode/Solution93.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class Solution93
+    {
+        public IList<string> RestoreIpAddresses(string s)
+        {
+            var result = new List<string>();
+            Backtrack(s, 0, new List<string>(), result);
+            return result;
+        }
+
+        private void Backtrack(string s, int index, List<string> segments, List<string> result)
+        {
+            if (segments.Count == 4)
+            {
+                if (index == s.Length)
+                {
+                    result.Add(string.Join(".", segments));
+                }
+                return;
+            }
+
+            for (int len = 1; len <= 3; len++)
+            {
+                if (index + len > s.Length) break;
+
+                string segment = s.Substring(index, len);
+                if (IsValid(segment))
+                {
+                    segments.Add(segment);
+                    Backtrack(s, index + len, segments, result);
+                    segments.RemoveAt(segments.Count - 1);
+                }
+            }
+        }
+
+        private bool IsValid(string segment)
+        {
+            if (segment.Length > 1 && segment[0] == '0') return false;
+            int value = int.Parse(segment);
+            return value >= 0 && value <= 255;
+        }
+    }
+}
